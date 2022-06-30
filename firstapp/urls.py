5da3bd137d7806cf31484b6f4444c9028897e8b9
Arjun Singh <admin@arjunsingh.com.np>
2022-06-30 21:07:48 +0545

    latest-6/30

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
-from .views import hello
+from .views import add, div, index, mul, sub
 
-urlpatterns = [path("join/", hello, name="home_page")]
+urlpatterns = [
+    path("", index, name="index_page"),
+    path("add/", add, name="add_page"),
+    path("sub/", sub, name="sub_page"),
+    path("mul/", mul, name="mul_page"),
+    path("div/", div, name="div_page"),
+]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 
-def hello(request):
+def index(request):
+
+    return render(request, "index.html")
+
+
+def add(request):
 
     try:
         a = int(request.GET.get("num1"))
@@ -11,4 +16,59 @@ def hello(request):
     except:
         c = "error"
 
-    return render(request, "home.html", {"sum": c})
+    ctx = {
+        "ans": c,
+        "btn_text": "Add",
+    }
+    return render(request, "calc.html", ctx)
+
+
+def sub(request):
+
+    try:
+        a = int(request.GET.get("num1"))
+        b = int(request.GET.get("num2"))
+
+        c = a - b
+    except:
+        c = "error"
+    ctx = {
+        "ans": c,
+        "btn_text": "Sub",
+    }
+    return render(request, "calc.html", ctx)
+
+
+def mul(request):
+
+    try:
+        a = int(request.GET.get("num1"))
+        b = int(request.GET.get("num2"))
+
+        c = a * b
+    except:
+        c = "error"
+
+    ctx = {
+        "ans": c,
+        "btn_text": "Mul",
+        "count": 458,
+    }
+    return render(request, "calc.html", ctx)
+
+
+def div(request):
+
+    try:
+        a = int(request.GET.get("num1"))
+        b = int(request.GET.get("num2"))
+
+        c = a / b
+    except:
+        c = "error"
+
+    ctx = {
+        "ans": c,
+        "btn_text": "Div",
+    }
+    return render(request, "calc.html", ctx)
